ab85e60ce4f111c9ffa608d93a577c44de6711a1
Vladimir Belich <belichvladimir@mail.ru>
2024-04-01 00:07:38 +0300

    упрощена проверка полей ввода координат

--- src/components/Leaflet/FormNewLeafletItem/FormNewLeafletItem.tsx
+++ src/components/Leaflet/FormNewLeafletItem/FormNewLeafletItem.tsx
@@ -5,15 +5,49 @@ import type { LatLngExpression } from 'leaflet';
 
 import './FormNewLeafletItem.css';
 
+const LATITUDE_DEGREE = 90;
+
+const LONGITUDE_DEGREE = 180;
+
 type FormNewLeafletItemProps = {
-  createNewLeafletItem: (coords:LatLngExpression & (LatLngExpression[] | LatLngExpression[][]),name:string) => void;
+  createNewLeafletItem: (
+    coords: LatLngExpression & (LatLngExpression[] | LatLngExpression[][]),
+    name: string
+  ) => void;
+};
+
+const checkValidInput = (
+  latitude: string,
+  longitude: string,
+  cbError: (error: boolean) => void
+): boolean => {
+  if (!latitude && !longitude) return false;
+  const inputLatitude = Number(latitude);
+  const inputLongitude = Number(longitude);
+  if (isNaN(inputLatitude) || isNaN(inputLongitude)) {
+    cbError(true);
+    return false;
+  }
+  if (
+    /^[+-]?\d+(\.\d+)?$/.test(latitude) &&
+    /^[+-]?\d+(\.\d+)?$/.test(longitude) &&
+    inputLatitude >= -LATITUDE_DEGREE &&
+    inputLatitude <= LATITUDE_DEGREE &&
+    inputLongitude >= -LONGITUDE_DEGREE &&
+    inputLongitude <= LONGITUDE_DEGREE
+  ) {
+    cbError(false);
+    return true;
+  }
+  cbError(true);
+  return false;
 };
 
 const FormNewLeafletItem: FC<FormNewLeafletItemProps> = ({
   createNewLeafletItem,
 }) => {
-  const [latitude, setLatitude] = useState<number | undefined | '-'>(undefined);
-  const [longitude, setLongitude] = useState<number | undefined | '-'>(undefined);
+  const [latitude, setLatitude] = useState<string>('');
+  const [longitude, setLongitude] = useState<string>('');
   const [name, setName] = useState('');
   const [errorInput, setErrorInput] = useState<boolean>(false);
 
@@ -21,87 +55,61 @@ const FormNewLeafletItem: FC<FormNewLeafletItemProps> = ({
     setName(event.target.value);
   };
   const handleChangeLatitude = (event: ChangeEvent<HTMLInputElement>) => {
-    if (!event.target.value) {
-      setLatitude(undefined);
-      return;
-    }
-    if (event.target.value ==='-') {
-      setLatitude(event.target.value);
-      return;
-    }
-    setErrorInput(false);
-    const value = Number(event.target.value);
-    if (isNaN(value)) {
-      setErrorInput(true);
-      return;
-    }
-    if (!(value>=-90 && value<=90)) {
-      setLatitude(value);
-      setErrorInput(true);
-      return;
-    }
-    setLatitude(value);
+    setLatitude(event.target.value);
   };
 
   const handleChangeLongitude = (event: ChangeEvent<HTMLInputElement>) => {
-    if (!event.target.value) {
-      setLongitude(undefined);
-      return;
-    }
-    if (event.target.value ==='-') {
-      setLongitude(event.target.value);
-      return;
-    }
-    setErrorInput(false);
-    const value = Number(event.target.value);
-    if (isNaN(value)) {
-      setErrorInput(true);
-      return;
-    }
-    if (!(value>=-180 && value<=180)) {
-      setLongitude(value);
-      setErrorInput(true);
-      return;
-    }
-    setLongitude(value);
+    setLongitude(event.target.value);
   };
 
-  const handleSubmit = (event:FormEvent<HTMLFormElement>) => {
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    if (latitude && longitude && name && !errorInput) {
-      createNewLeafletItem([latitude,longitude] as LatLngExpression & (LatLngExpression[] | LatLngExpression[][]) ,name);
+    if (latitude === undefined && longitude === undefined) return;
+
+    if (checkValidInput(latitude, longitude, setErrorInput) && name) {
+      createNewLeafletItem(
+        [Number(latitude), Number(longitude)] as LatLngExpression &
+          (LatLngExpression[] | LatLngExpression[][]),
+        name
+      );
+      setLatitude('');
+      setLongitude('');
+      setName('');
     }
-  
   };
 
   return (
     <form className={cnFormNewLeaflet()} onSubmit={handleSubmit}>
       <div className={cnFormNewLeaflet('GroupInput')}>
-          <input
+        <input
           className={cnFormNewLeaflet('Input')}
           type="text"
           onChange={handleChangeName}
           value={name}
           placeholder="Имя"
-          />
-          <input
-            className={cnFormNewLeaflet('Input')}
-            type="text"
-            onChange={handleChangeLatitude}
-            value={latitude}
-            placeholder="Широта"
-          />
-          <input
+        />
+        <input
+          className={cnFormNewLeaflet('Input')}
+          type="text"
+          onChange={handleChangeLatitude}
+          value={latitude}
+          placeholder="Широта"
+        />
+        <input
           className={cnFormNewLeaflet('Input')}
           type="text"
           onChange={handleChangeLongitude}
           value={longitude}
           placeholder="Долгота"
-          />
-        <p className={errorInput ? 'error' : 'error hidden'}>Неверный формат координат</p>
+        />
+        <p className={errorInput ? 'error' : 'error hidden'}>
+          Неверный формат координат
+        </p>
       </div>
-      <button className={cnFormNewLeaflet('Button')} type="submit" disabled={errorInput}>Сохранить</button>
+      <button className={cnFormNewLeaflet('Button')} type="submit">
+        Сохранить
+      </button>
     </form>
   );
 };
